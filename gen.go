eaf8781427d6ed89ffdce52562ed6f6377f5c1b6
Noah Zoschke <noah@mixable.net>
2025-04-14 19:13:50 -0700

    extract tmpl

--- gen.go
+++ gen.go
@@ -64,7 +64,7 @@ func Gen(ctx context.Context, req *plugin.GenerateRequest) (*plugin.GenerateResp
 		return nil, errors.WithStack(err)
 	}
 
-	q, err := template.New("queries.tmpl").Funcs(funcMap).ParseFS(tmpl, "queries.tmpl")
+	q, err := template.New("queries.tmpl").Funcs(funcMap).ParseFS(tmpl, "exec.tmpl", "queries.tmpl")
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
