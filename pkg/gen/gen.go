892ebb1c5e9dd59246a8f13b0760a0f79a8a444b
Noah Zoschke <noah@mixable.net>
2025-04-15 23:33:23 -0700

    move funcs

--- pkg/gen/gen.go
+++ pkg/gen/gen.go
@@ -21,104 +21,17 @@ func Gen(ctx context.Context, req *plugin.GenerateRequest) (*plugin.GenerateResp
 	slog.Info("gen", "req", req)
 
 	funcMap := template.FuncMap{
-		"camel": strcase.ToCamel,
-		"bindval": func(ps []*plugin.Parameter, i int32) string {
-			p := ps[i-1]
-
-			cast := func(v string) string {
-				switch gotype(p.Column) {
-				case "time.Time":
-					return fmt.Sprintf(`%s.Format("2006-01-02 15:04:05")`, v)
-				default:
-					return v
-				}
-			}
-
-			switch len(ps) {
-			case 1:
-				// id
-				return cast(p.Column.Name)
-			default:
-				// in.Id
-				return cast(fmt.Sprintf("in.%s", strcase.ToCamel(p.Column.Name)))
-			}
-		},
-		"dbtype": dbtype,
-		"gotype": gotype,
-		"inarg": func(name string, ps []*plugin.Parameter) string {
-			switch len(ps) {
-			case 0:
-				return ""
-			case 1:
-				// id int
-				p := ps[0]
-				return fmt.Sprintf(", %s %s", p.Column.Name, gotype(p.Column))
-			default:
-				// in ContactCreateIn
-				return fmt.Sprintf(", in %sIn", name)
-			}
-		},
-		"outarg": func(name string, cs []*plugin.Column) string {
-			switch len(cs) {
-			case 0:
-				return ""
-			case 1:
-				// int
-				c := cs[0]
-				return fmt.Sprintf("%s, ", gotype(c))
-			default:
-				// *ContactCreateOut
-				return fmt.Sprintf("*%sOut, ", name)
-			}
-		},
-		"lower": strings.ToLower,
-		"retempty": func(name string, cs []*plugin.Column) string {
-			switch len(cs) {
-			case 0:
-				return ""
-			case 1:
-				c := cs[0]
-				switch gotype(c) {
-				case "[]byte":
-					return "nil"
-				case "time.Time":
-					return "time.Time{}"
-				case "text":
-					return `""`
-				case "int64", "float64":
-					return "0"
-				default:
-					return "nil"
-				}
-			default:
-				return "nil"
-			}
-		},
-		"retval": func(cs []*plugin.Column, i int) string {
-			c := cs[i]
-
-			switch gotype(c) {
-			case "[]byte":
-				return fmt.Sprintf("[]byte(stmt.ColumnText(%d))", i)
-			case "time.Time":
-				return fmt.Sprintf("timeParse(stmt.ColumnText(%d))", i)
-			default:
-				return fmt.Sprintf("stmt.Column%s(%d)", dbtype(c.Type.Name), i)
-			}
-		},
-		"singular": pl.Singular,
-		"timeimport": func(c *plugin.Catalog) string {
-			for _, s := range c.Schemas {
-				for _, t := range s.Tables {
-					for _, c := range t.Columns {
-						if gotype(c) == "time.Time" {
-							return `"time"`
-						}
-					}
-				}
-			}
-			return ""
-		},
+		"camel":      strcase.ToCamel,
+		"bindval":    bindval,
+		"dbtype":     dbtype,
+		"gotype":     gotype,
+		"inarg":      inarg,
+		"outarg":     outarg,
+		"lower":      strings.ToLower,
+		"retempty":   retempty,
+		"retval":     retval,
+		"singular":   pl.Singular,
+		"timeimport": timeimport,
 	}
 
 	res := &plugin.GenerateResponse{
@@ -158,6 +71,28 @@ func Gen(ctx context.Context, req *plugin.GenerateRequest) (*plugin.GenerateResp
 	return res, nil
 }
 
+func bindval(ps []*plugin.Parameter, i int32) string {
+	p := ps[i-1]
+
+	cast := func(v string) string {
+		switch gotype(p.Column) {
+		case "time.Time":
+			return fmt.Sprintf(`%s.Format("2006-01-02 15:04:05")`, v)
+		default:
+			return v
+		}
+	}
+
+	switch len(ps) {
+	case 1:
+		// id
+		return cast(p.Column.Name)
+	default:
+		// in.Id
+		return cast(fmt.Sprintf("in.%s", strcase.ToCamel(p.Column.Name)))
+	}
+}
+
 func dbtype(t string) string {
 	switch strings.ToLower(t) {
 	case "integer":
@@ -187,4 +122,81 @@ func gotype(c *plugin.Column) string {
 	default:
 		return "[]byte"
 	}
+}
+
+func inarg(name string, ps []*plugin.Parameter) string {
+	switch len(ps) {
+	case 0:
+		return ""
+	case 1:
+		// id int
+		p := ps[0]
+		return fmt.Sprintf(", %s %s", p.Column.Name, gotype(p.Column))
+	default:
+		// in ContactCreateIn
+		return fmt.Sprintf(", in %sIn", name)
+	}
+}
+
+func outarg(name string, cs []*plugin.Column) string {
+	switch len(cs) {
+	case 0:
+		return ""
+	case 1:
+		// int
+		c := cs[0]
+		return fmt.Sprintf("%s, ", gotype(c))
+	default:
+		// *ContactCreateOut
+		return fmt.Sprintf("*%sOut, ", name)
+	}
+}
+
+func retempty(name string, cs []*plugin.Column) string {
+	switch len(cs) {
+	case 0:
+		return ""
+	case 1:
+		c := cs[0]
+		switch gotype(c) {
+		case "[]byte":
+			return "nil"
+		case "time.Time":
+			return "time.Time{}"
+		case "text":
+			return `""`
+		case "int64", "float64":
+			return "0"
+		default:
+			return "nil"
+		}
+	default:
+		return "nil"
+	}
+}
+
+func retval(cs []*plugin.Column, i int) string {
+	c := cs[i]
+
+	switch gotype(c) {
+	case "[]byte":
+		return fmt.Sprintf("[]byte(stmt.ColumnText(%d))", i)
+	case "time.Time":
+		return fmt.Sprintf("timeParse(stmt.ColumnText(%d))", i)
+	default:
+		return fmt.Sprintf("stmt.Column%s(%d)", dbtype(c.Type.Name), i)
+	}
+}
+
+func timeimport(c *plugin.Catalog) string {
+	for _, s := range c.Schemas {
+		for _, t := range s.Tables {
+			for _, c := range t.Columns {
+				if gotype(c) == "time.Time" {
+					return `"time"`
+				}
+			}
+		}
+	}
+	return ""
 }
